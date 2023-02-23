0898ecc90cbbf54726272409e042788870185996
Linting <303156351@qq.com>
2023-02-24 01:30:17 +0800

    deploy vue-admin

---
+++ assets/Author-e9026832.js
@@ -0,0 +1,1 @@
+import{d as e,o,c as n,g as s,_ as c}from"./index-5768c456.js";const _={class:"content-container"},a=e({__name:"Author",setup(r){return s().appContext.config.globalProperties.$axios.get("/author/get").then(t=>{console.log(t)}),(t,l)=>(o(),n("div",_))}});const d=c(a,[["__scopeId","data-v-064cf32c"]]);export{d as default};
