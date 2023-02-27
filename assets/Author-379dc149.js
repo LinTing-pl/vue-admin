c31653e9add3db1fc98331447931838e3bd794bd
Linting <303156351@qq.com>
2023-02-27 21:13:24 +0800

    Menu Component 2.0

---
+++ assets/Author-379dc149.js
@@ -0,0 +1,1 @@
+import{d as e,c as o,g as n,o as s,_ as c}from"./index-dccb7f5c.js";const _={class:"content-container"},a=e({__name:"Author",setup(r){return n().appContext.config.globalProperties.$axios.get("/author/get").then(t=>{console.log(t)}),(t,l)=>(s(),o("div",_))}});const d=c(a,[["__scopeId","data-v-064cf32c"]]);export{d as default};
